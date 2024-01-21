871176ac2dd1f7f159d9979ab10cef2888c1cb11
skbali <surinder.bali@gmail.com>
2024-01-20 19:43:01 -0500

    optimize

--- running-ec2/code/main.go
+++ running-ec2/code/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -22,27 +23,37 @@ func init() {
 }
 
 func HandleRequest() ([]string, error) {
+	var nextToken *string
+	var status []string
 
-	result, err := client.DescribeInstances(context.TODO(), &ec2.DescribeInstancesInput{
-		Filters: []types.Filter{
-			{
-				Name: aws.String("instance-state-name"),
-				Values: []string{
-					"running",
+	for {
+		result, err := client.DescribeInstances(context.TODO(), &ec2.DescribeInstancesInput{
+			Filters: []types.Filter{
+				{
+					Name: aws.String("instance-state-name"),
+					Values: []string{
+						"running",
+					},
 				},
 			},
-		},
-	})
+			MaxResults: aws.Int32(6),
+			NextToken:  nextToken,
+		})
 
-	if err != nil {
-		return []string{}, err
-	}
-
-	var status []string
-	for _, r := range result.Reservations {
-		for _, ins := range r.Instances {
-			status = append(status, fmt.Sprintf("InstanceID: %v State: %v", *ins.InstanceId, ins.State.Name))
+		if err != nil {
+			return []string{}, err
 		}
+
+		for _, r := range result.Reservations {
+			for _, ins := range r.Instances {
+				status = append(status, fmt.Sprintf("InstanceID: %v State: %v", *ins.InstanceId, ins.State.Name))
+			}
+		}
+
+		if result.NextToken == nil {
+			break
+		}
+		nextToken = result.NextToken
 
 	}
 
